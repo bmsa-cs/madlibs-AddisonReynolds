be068c006984d05360af9351dffe90bd6d7f44a5
Addison Reynolds <areynolds24@biomedscienceacademy.org>
2020-11-08 23:47:32 +0000

    I added code relating to creating a Mad Libs

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Addison Reynolds 
+Period/Core: Cohort H, Core 7
 """
+print("Let's play Silly Sentences!\n Story catagory: Swimming") 
+adjective=input("Enter an adjective ")
+noun=input("Enter a noun ")
+proper_noun=input("Enter a proper noun ")
+noun_two=input("Enter a noun(Plural) ")
+place=input("Enter a place ")
+print("\tSwimming is a very " + adjective + " sport. There are 4 strokes and they are freestyle, backstroke, breastroke, and " + noun + ".\n\tSome of the most famous swimmers are Katie Ledecky, Michel Pheleps, and " + proper_noun + ". These famous swimmers are well known for swimming in the Olympics.\n\tBoys, girls, and " + noun_two + " love to swim. They can swim in places like a pool, lake, and a/the " + place + ".")
